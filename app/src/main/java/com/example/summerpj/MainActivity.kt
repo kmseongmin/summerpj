494006e74f6a784ee024cc9370ee6b885828b243
kimseongmin <newsungk7@naver.com>
2023-07-25 13:18:06 +0900

    login 레이아웃

--- app/src/main/java/com/example/summerpj/MainActivity.kt
+++ app/src/main/java/com/example/summerpj/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.summerpj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Emailedt:EditText
+    lateinit var Passwordedt:EditText
+    lateinit var loginbtn:Button
+    lateinit var Registerbtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
