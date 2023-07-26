f2a7a26d12e8ab79dcc2c279fabd74ce2f86af97
kimseongmin <newsungk7@naver.com>
2023-07-26 14:21:47 +0900

    구글api 매니페스트, gradle 설정 메인룸 엑티비티에 툴바생성

---
+++ app/src/main/java/com/example/summerpj/MainroomActivity.kt
@@ -0,0 +1,40 @@
+package com.example.summerpj
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.summerpj.databinding.ActivityMainBinding
+
+class MainroomActivity : AppCompatActivity() {
+    lateinit var toolbar:Toolbar
+    lateinit var main_drawer_layout:DrawerLayout
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainroom)
+
+        //메인 drawer
+        main_drawer_layout=findViewById(R.id.main_drawer_layout)
+
+        //툴바 설정
+        toolbar = findViewById(R.id.main_layout_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_24) // 홈버튼 이미지 변경
+    }
+
+    //네비게이션 드로어 홈버튼으로 열기
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{ // 메뉴 버튼
+
+                main_drawer_layout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
