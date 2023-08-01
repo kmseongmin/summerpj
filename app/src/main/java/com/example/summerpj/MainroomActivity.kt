5dcec94ad4bf37a972552156f3112718f49152af
kimseongmin <newsungk7@naver.com>
2023-08-01 21:08:47 +0900

    1. 네비게이션드로어 myinfo에 연결 2. 구글맵 위치 권한체크 3. 구글맵 내위치 띄우고 내위치 버튼 활성화

--- app/src/main/java/com/example/summerpj/MainroomActivity.kt
+++ app/src/main/java/com/example/summerpj/MainroomActivity.kt
@@ -1,22 +1,39 @@
 package com.example.summerpj
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.navigation.NavigationView
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.net.PlacesClient
 
 
 class MainroomActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var toolbar:Toolbar
     lateinit var main_drawer_layout:DrawerLayout
     private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
+    private var googleMap: GoogleMap? = null
+    lateinit var main_nav:NavigationView
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private var locationPermissionGranted = false
+
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +42,111 @@ class MainroomActivity : AppCompatActivity(), OnMapReadyCallback {
         //메인 drawer
         main_drawer_layout=findViewById(R.id.main_drawer_layout)
 
+        //메인 nav
+        main_nav = findViewById(R.id.main_navigationView)
+
         //툴바 설정
         toolbar = findViewById(R.id.main_layout_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_24) // 홈버튼 이미지 변경
 
+        //place api 추가
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, "AIzaSyB57FGSwBEESpdkLu9lM9H_kOt7DMyAfAY")
+        }
+
+        //네비게이션 아이템 SelectedListener
+        main_nav.setNavigationItemSelectedListener {menuItem->
+            when (menuItem.itemId){
+                R.id.my_account->{
+                    val intent = Intent(this, myinfo::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> {false}
+            }
+        }
 
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
+        checkLocationPermission()
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        this.googleMap = googleMap
-        // 이제 구글 지도 객체를 사용하여 지도를 커스터마이징하고 작업할 수 있습니다.
-        // 예를 들어 마커 추가, 맵 타입 설정, 위치 활성화 등을 할 수 있습니다.
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            locationPermissionGranted = true
+            initializeMap()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 사용자가 위치 권한을 허용한 경우
+                locationPermissionGranted = true
+                initializeMap()
+            } else {
+                // 사용자가 위치 권한을 거부한 경우
+                Toast.makeText(this, "위치 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initializeMap() {
+        mapView.onCreate(Bundle())
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.googleMap = map
+        if (locationPermissionGranted) {
+            // 위치 권한이 허용된 경우에만 내 위치를 표시하도록 호출
+            enableMyLocation()
+        }
+    }
+
+    private fun enableMyLocation() {
+        if (googleMap != null) {
+            try {
+                googleMap?.isMyLocationEnabled = true
+                getCurrentLocation()
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getCurrentLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        try {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val currentLocation = LatLng(location.latitude, location.longitude)
+                    googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+                }
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 
     //네비게이션 드로어 홈버튼으로 열기
